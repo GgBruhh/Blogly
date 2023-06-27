f41926a59e9c870d98fb175211dd6a4030a35d33
Camryn Scott <camryn1242@yahoo.com>
2023-06-27 19:40:03 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SECRET_KEY'] = 'HeisKing!'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
+db.create_all()
+
+
+
---
+++ models.py
@@ -0,0 +1,23 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+DEFAULT_IMAGE_URL = "https://www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person-generic--public-domain--21.png"
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    first_name = db.Column(db.String,
+                           nullable=False)
+    last_name = db.Column(db.String,
+                           nullable=False)
+    image_url = db.Column(db.String,
+                           nullable=False,
+                           default=DEFAULT_IMAGE_URL)
+    
