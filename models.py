a0b5077096ea8ec2cab3fe6a08f744c40aa85b7a
Camryn Scott <camryn1242@yahoo.com>
2023-06-28 17:30:59 -0400

    added Post Model

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 db = SQLAlchemy()
 
@@ -13,11 +14,27 @@ class User(db.Model):
     id = db.Column(db.Integer,
                    primary_key=True,
                    autoincrement=True)
-    first_name = db.Column(db.String,
+    first_name = db.Column(db.Text,
                            nullable=False)
-    last_name = db.Column(db.String,
+    last_name = db.Column(db.Text,
                            nullable=False)
-    image_url = db.Column(db.String,
+    image_url = db.Column(db.Text,
                            nullable=False,
                            default=DEFAULT_IMAGE_URL)
-    
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    title = db.Column(db.Text,
+                      nullable=False)
+    content = db.Column(db.Text,
+                        nullable=False)
+    created_at = db.Column(db.DateTime,
+                            nullable=False,
+                            default=datetime.datetime.now)
+    user_id = db.Column(db.Integer, 
+                        db.ForeignKey('users.id'), 
+                        nullable=False)
